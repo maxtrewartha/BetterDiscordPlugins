49978b2f4e6cfaf4ad1be656dd11c422e5fc9ecf
Max 3 <max@trewartha.co.uk>
2022-10-17 00:01:20 +0100

    Plugin now changed username on new message

--- PronounDB-v2/PronounDB-v2.plugin.js
+++ PronounDB-v2/PronounDB-v2.plugin.js
@@ -29,23 +29,121 @@ const Pronouns = {
 	avoid: "Avoid",
 };
 
-async function getPronouns(userid) {}
+module.exports = (() => {
+	const config = {
+		info: {
+			name: "PronounDB-v2",
+			authors: [
+				{
+					name: "mtht",
+					discord_id: "261178034587959317",
+					github_username: "maxtrewartha",
+					twitter_username: "sneetsnart",
+				},
+			],
+			version: "0.0.1",
+			description: "Shows you the pronoun of a user next to their name.",
+			github: "https://github.com/maxtrewartha/BetterDiscordPlugins",
+		},
+		changelog: [
+			{ title: "New Stuff", items: ["Added more settings", "Added changelog"] },
+			{ title: "Bugs Squashed", type: "fixed", items: ["React errors on reload"] },
+			{ title: "Improvements", type: "improved", items: ["Improvements to the base plugin"] },
+			{
+				title: "On-going",
+				type: "progress",
+				items: ["More modals and popouts being added", "More classes and modules being added"],
+			},
+		],
+		main: "PronounDB-v2.plugin.js",
+	};
 
-module.exports = class PronounDB_v2 {
-	constructor(meta) {
-		// Do stuff in here before starting
-	}
+	return !global.ZeresPluginLibrary
+		? class {
+				constructor() {
+					this._config = config;
+				}
+				getName() {
+					return config.info.name;
+				}
+				getAuthor() {
+					return config.info.authors.map((a) => a.name).join(", ");
+				}
+				getDescription() {
+					return config.info.description;
+				}
+				getVersion() {
+					return config.info.version;
+				}
+				load() {
+					BdApi.showConfirmationModal(
+						"Library Missing",
+						`The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`,
+						{
+							confirmText: "Download Now",
+							cancelText: "Cancel",
+							onConfirm: () => {
+								require("request").get(
+									"https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js",
+									async (error, response, body) => {
+										if (error)
+											return require("electron").shell.openExternal(
+												"https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
+											);
+										await new Promise((r) =>
+											require("fs").writeFile(
+												require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"),
+												body,
+												r
+											)
+										);
+									}
+								);
+							},
+						}
+					);
+				}
+				start() {}
+				stop() {}
+		  }
+		: (([Plugin, Api]) => {
+				const plugin = (Plugin, Library) => {
+					const { Logger, Patcher, DiscordModules } = Library;
 
-	start() {
-		// Called when the plugin is activated (including after reloads)
-		BdApi.alert("Hello World!", "This is my first plugin!");
-	}
+					return class PronounDBv2 extends Plugin {
+						constructor() {
+							super();
+						}
 
-	observer(event) {
-		//console.log(event["addedNodes"][0].getAttribute("id"));
-	}
+						observer(e) {
+							if (!e.addedNodes.length || !(e.addedNodes[0] instanceof Element)) return;
+							if (e.addedNodes[0].querySelector("h3")) {
+								let url = new URL(e.addedNodes[0].getElementsByTagName("img")[0].getAttribute("src"));
+								let id = url.pathname.split("/")[2];
+								Logger.log(id);
+								e.addedNodes[0].getElementsByTagName("span")[1].innerText += ` [${id}]`;
+							}
+						}
 
-	stop() {
-		// Called when the plugin is deactivated
-	}
-};
+						onStart() {
+							Logger.log("Started");
+							Patcher.before(Logger, "log", (t, a) => {
+								a[0] = "Patched Message: " + a[0];
+							});
+
+							// Patcher.after(DiscordModules.MessageActions, "receiveMessage", (t, a) => {
+							// 	Logger.log(JSON.stringify(t));
+							// 	Logger.log(JSON.stringify(a));
+							// });
+						}
+
+						onStop() {
+							Logger.log("Stopped");
+							Patcher.unpatchAll();
+						}
+					};
+				};
+				return plugin(Plugin, Api);
+		  })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
